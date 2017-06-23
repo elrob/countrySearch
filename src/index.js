f6e44c73785c1f00f2c1ad8b3a810cc9f10c61a8
Rob Speller <rspeller@thoughtworks.com>
2017-06-23 22:53:39 +0100

    Create SearchBox component and display

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-import { countryList } from './countryList';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import countryList from './countryList';
+import SearchBox from './SearchBox';
 
-console.log(countryList);
+class App extends Component {
+  countrySearch(term) {
+    console.log(term);
+  }
+
+  render() {
+    return <SearchBox onSearchTermChange={this.countrySearch} />;
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
